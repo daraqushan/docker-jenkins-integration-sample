ab8f8202ad25e3e96bdaf816c9b46288190f19f4
Fatima Daraqushan (Tech Consultantl) <fatima.d@cgg.gov.in>
2024-06-10 15:17:36 +0530

    Second Commit

--- src/main/java/cgg/docker/jenkins/App.java
+++ src/main/java/cgg/docker/jenkins/App.java
@@ -4,10 +4,9 @@ package cgg.docker.jenkins;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Testing jenkins ...");
     }
 }
